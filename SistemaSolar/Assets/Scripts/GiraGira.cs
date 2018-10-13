2418ea25fbc1ee1fe333d3f0520398886d7baa53
AlexandreLadeira <32870665+AlexandreLadeira@users.noreply.github.com>
2018-10-13 15:30:01 -0300

    Sol e todos os planetas adicionados
    Os anéis de Saturno já estão implementados, faltando completar todas as luas, colocar as texturas em todos os planetas, as muiscas e tratar o minimapa e a intereção.

---
+++ SistemaSolar/Assets/Scripts/GiraGira.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraGira : MonoBehaviour {
+
+
+    public float x = 0, y = 0, z = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update() { 
+        Vector3 rotacao = new Vector3(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime); // Time.deltaTime = Tempo desde o último update
+        transform.Rotate(rotacao);
+	}
+}
